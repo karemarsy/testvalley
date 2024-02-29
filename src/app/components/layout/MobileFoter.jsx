c1eaf55ec5d33df2d6e7aaf0bbaafe0bef8af2d7
Arsene <arsene@Arsenes-MacBook-Pro-2.local>
2024-02-29 12:05:22 +0200

    fix: add link on logo

--- src/app/components/layout/MobileFoter.jsx
+++ src/app/components/layout/MobileFoter.jsx
@@ -8,7 +8,7 @@ export default function MobileFoter() {
     <div className="md:hidden">
       <div className="w-full bg-white text-[#333333]">
         <div className="flex w-100 p-1 items-center relative mx-auto h-[55px]">
-          <LogoMobile className="w-[225.71px] cursor-pointer block p-0 ml-5" />
+          <a href="/"><LogoMobile className="w-[225.71px] cursor-pointer block p-0 ml-5" /></a>
           <div className="flex items-center gap-3 px-3 text-[#333333] w-full justify-end">
             <Bell  className="cursor-pointer"/>
             <SearchIcon className="cursor-pointer "/>
--- src/app/components/layout/Navbar.jsx
+++ src/app/components/layout/Navbar.jsx
@@ -10,7 +10,7 @@ function navbar() {
     <nav className=" hidden md:block w-full h-[72px] bg-white sticky border-b border-solid border-b-[#EEEEEE]">
       <div className="w-[960px] m-auto flex justify-between items-center align-middle h-full">
         <div className="flex items-center justify-center ">
-          <Logo className="mr-4 w-[128.345px] h-[25.093px]" />
+          <a href="/"><Logo className="mr-4 w-[128.345px] h-[25.093px]" /></a>
           <div className="text-[#99CC99] cursor-pointer flex items-center">
             <span className="mr-4 ">
               <MenuIcon />
